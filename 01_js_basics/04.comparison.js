0ac2ae247c8620d5c7805d34b76804144d8e654e
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2024-12-20 17:26:53 +0000

    Comparison of data

---
+++ 01_js_basics/04.comparison.js
@@ -0,0 +1,15 @@
+console.table([2>1 , 2>=5 , 1<2 , 1 != 2])
+
+console.table(["2" > 1 , "1" < 2])
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+
+
+console.log(undefined >= 0)
+console.log(undefined <= 0)
+console.log(undefined == 0)
+
+console.log("2" == 2)
+console.log("2" === 2)
