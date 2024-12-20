86bd0eb6c95c46eb282037482893f706c1ffd30e
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2024-12-20 13:57:30 +0000

    01 basics

---
+++ 01_js_basics/01_variables.js
@@ -0,0 +1,10 @@
+console.log("shreya");
+
+const account_Id = 123987
+let accountEmail = "shreya@github.com"
+var accountPassword = "sh1234"
+accountCity = "Mumbai.."
+
+// account_Id = 2   not allowed
+
+console.log(account_Id);
