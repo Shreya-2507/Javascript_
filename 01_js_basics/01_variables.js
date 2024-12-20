f7e8203b2988abb326c0e4577fee7b42c0056a51
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2024-12-20 14:52:02 +0000

    datatypes

--- 01_js_basics/01_variables.js
+++ 01_js_basics/01_variables.js
@@ -5,6 +5,20 @@ let accountEmail = "shreya@github.com"
 var accountPassword = "sh1234"
 accountCity = "Mumbai.."
 
+console.log(account_Id);
+
 // account_Id = 2   not allowed
 
-console.log(account_Id);
+accountEmail = "github@gmail.com"
+accountPassword = "12345t"
+accountCity = "Bandra"
+let accountState 
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([ account_Id , accountEmail , accountPassword , accountCity , accountState]);
+
+
---
+++ 01_js_basics/02.datatypes.js
@@ -0,0 +1,12 @@
+"use strict";  // treat all js code as newer version .
+
+// alert("shreya")            we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("shreya")
+
+// datatype => object
+
+console.log(typeof (null))
+console.log(typeof (undefined))
