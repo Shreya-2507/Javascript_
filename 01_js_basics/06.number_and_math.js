88f1e555ef5d69cc7bee8c95202f5323d987f4e5
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2024-12-24 15:38:16 +0000

    concepts of Number & math

---
+++ 01_js_basics/06.number_and_math.js
@@ -0,0 +1,49 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(908)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const hundreads = 10000000
+// console.log(hundreads.toLocaleString('en-IN'));
+
+// const preciseNum = 587.76
+// console.log(preciseNum.toPrecision(4));
+
+
+// console.log(balance.toExponential(4));
+
+
+//                       *********************** Math **********************
+
+const number = 40.8
+
+console.log(Math.ceil(number));
+console.log(Math.floor(number));
+console.log(Math.round(number));
+console.log((Math.min(1,2,3,3,4)));
+console.log(Math.max(1,2,3,4));
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
