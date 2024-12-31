188ddad4b61e0eee4899fc617fb921ffe334c5cd
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2024-12-31 13:06:02 +0000

    switch logic

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("Aprin");
+        break;
+
+    default:
+        console.log("no one value is matched")
+        break;
+};
+
+const week = "friday"
+
+switch (week) {
+    case "monday":
+        console.log("Monday")
+        break;
+    
+    case "tuesday":
+        console.log("Tueday")
+        break;
+    case "wednesday":
+        console.log("Wednesday")
+        break;
+    case "thursday":
+        console.log("Thurday")
+        break;
+    case "friday":
+        console.log("Friday")
+        break;
+    case "saturday":
+        console.log("Saturday")       
+        break;
+    default:
+        console.log("Sunday")
+        break;
+}
