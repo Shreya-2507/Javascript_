fa7bf1945e12d389f534bd68b50c8fe39992b7f8
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2025-01-03 13:06:44 +0000

    concept of while and do while loop

---
+++ 05_iterators/while_loop.js
@@ -0,0 +1,31 @@
+//while
+
+let score = 2
+
+while (score<=20) {
+    console.log(`score : ${score}`);
+    score+=2
+    
+    
+}
+
+let myArray = ["python","cpp","java","js","php"]
+
+let start = 0
+
+while (start < myArray.length) {
+    console.log(`Value of ${start} is ${myArray[start]}`);
+    start++
+    
+    
+}
+
+// do while
+
+let marks = 0
+
+do {
+    console.log(`marks is increased by ${marks}`);
+    marks++
+    
+} while (marks<=10);
