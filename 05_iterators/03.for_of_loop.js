7aece6e852fd43492e01e682e0f1d6f95b4f6386
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2025-01-04 06:12:38 +0000

    for of loop on an array , string , function  and map

---
+++ 05_iterators/03.for_of_loop.js
@@ -0,0 +1,54 @@
+// for of loop
+
+let country = ["India" , "America" ,"Ingland","Russia"]
+
+for (const val of country) {
+    console.log(val);
+    
+    
+}
+
+// for of with string
+
+let greeting = "hello , javascript"
+
+// for (const greet of greeting) {
+//     if (greet = "") {
+//         continue;
+    
+//     }
+//     console.log(`char of string : ${greet}`);
+    
+    
+// }
+
+// Maps
+const map = new Map()                   // unique key container
+map.set('IN'," India")
+map.set('Br',"Brazil")
+map.set('Au',"Australia")
+map.set('UK',"United Kingdom")
+
+// console.log(map);
+
+for (const key of map) {
+    console.log(key);
+       
+}
+
+for (const [key,value] of map) {
+    console.log(key ,':-',value);
+        
+}
+
+const object = {
+    game : "PUBG",
+    Anime : "Solo leveling"
+}
+
+// for of loop not working on object 
+
+// for (const [key,value] of object) {
+//      console.log(key,value);
+     
+// }
