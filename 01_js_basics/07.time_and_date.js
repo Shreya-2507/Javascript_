4454c5882bba85ca056c3780a149a25182a5f858
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2025-01-04 05:08:29 +0000

    concepts of time and date

--- 01_js_basics/07.time_and_date.js
+++ 01_js_basics/07.time_and_date.js
@@ -12,18 +12,19 @@ console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 
 console.log(myDate.toLocaleTimeString())
+"\n"
 
 // in js months starts from 0 (or index 0)
-let newDate = new Date(2025, 0 , 1)
+let newDate = new Date(2025, 0 , 4)
 console.log(newDate.toDateString());
 
-let date = new Date(2024 , 11 , 27 , 6 , 44 ,45)
+let date = new Date(2024 , 1 , 4 , 10 , 36,4)
 console.log(date.toLocaleString ());
 
 let _date = new Date ("12-27-2024")
 console.log(_date.toLocaleString('default',{weekday:"narrow"}));
 
-// for milisecond comarison
+// for milisecond comparison
 let myTimeStamp =  Date.now()
 console.log(myTimeStamp);
 
